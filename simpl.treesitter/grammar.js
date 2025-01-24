08b8c0bb4ae6fb3d57fe2774650740271646396c
zuzmuz <hamadehzaher0@gmail.com>
2025-01-24 19:30:44 +0200

    removed folder and added llvm

--- simpl.treesitter/grammar.js
+++ simpl.treesitter/grammar.js
@@ -10,9 +10,10 @@
 
 
 const PREC = {
-  UNARY: 6,
-  MULT: 5,
-  ADD: 4,
+  UNARY: 10,
+  MULT: 8,
+  ADD: 6,
+  PIPE: 5,
   COMP: 3,
   AND: 2,
   OR: 1,
@@ -26,7 +27,7 @@ module.exports = grammar({
 
   rules: {
     source_file: $ => repeat($._statement),
-    comment: _ => seq('* ', /(\\+(.|\r?\n)|[^\\\n])*/),
+    comment: _ => seq('** ', /(\\+(.|\r?\n)|[^\\\n])*/),
     _statement: $ => choice(
       $._declaration,
       $.expression,
@@ -66,6 +67,7 @@ module.exports = grammar({
     param_list: $ => repeat1($.param_declaration),
     param_declaration: $ => seq(
       field("name", $.field_identifier),
+      ":",
       field("type", $.type_identifier),
     ),
 
@@ -76,25 +78,27 @@ module.exports = grammar({
     field_identifier: $ => /[a-z][a-zA-Z0-9_]*/,
 
     inline_function_declaration: $ => seq(
-      '(',
+      '{',
       repeat($.param_declaration),
-      ')',
+      '}',
       $.type_identifier,
     ),
 
     expression: $ => choice(
       $.single_expression,
-      // $.unary_expression,
-      // $.binary_expression,
+      $.unary_expression,
+      $.binary_expression,
       $.pipe_expression,
       $.call_expression,
-      // $.parenthised_expression,
+      $.lambda_expression,
+      $.parenthised_expression,
     ),
 
     single_expression: $ => choice(
       $.int_literal,
       $.float_literal,
       $.string_literal,
+      $.array_literal,
       $.true,
       $.false,
       $.field_identifier
@@ -106,22 +110,27 @@ module.exports = grammar({
       ')',
     ),
 
-    pipe_expression: $ => prec.left(10, seq(
+    pipe_expression: $ => prec.left(PREC.PIPE, seq(
         field("field", $.expression),
         '|',
         $.call_expression,
     )),
 
-    call_expression: $ => prec.left(11, seq(
+    call_expression: $ => prec.left(PREC.PIPE, seq(
         field("function", $.field_identifier),
         field("params", optional($.param_list_call)),
     )),
 
-    param_list_call: $ => prec.left(12, repeat1($.param_definition)),
+    param_list_call: $ => prec.left(PREC.PIPE, repeat1($.param_definition)),
 
     param_definition: $ => seq(
       field("name", $.field_identifier),
-      field("value", $.single_expression),
+      ":",
+      field("value", choice(
+        $.single_expression,
+        $.parenthised_expression,
+        $.lambda_expression,
+      )),
     ),
 
     unary_expression: $ => prec.left(PREC.UNARY,
@@ -130,6 +139,15 @@ module.exports = grammar({
         field("operand", $.expression),
       )
     ),
+
+    lambda_expression: $ => seq(
+      '{',
+      optional(seq('|', $.lambda_argument_list, '|')),
+      $.expression,
+      '}'
+    ),
+
+    lambda_argument_list: $ => repeat1($.field_identifier),
 
     binary_expression: $ => choice(
       prec.left(PREC.MULT,
@@ -174,6 +192,7 @@ module.exports = grammar({
     int_literal: $ => /\d+/,
     float_literal: $ => /\d+\.\d+/,
     string_literal: $ => /"[^"]*"/,
+    array_literal: $ => seq('[', repeat($.expression), ']'),
 
     multiplicative_operator: $ => choice('*', '/', '%'),
     additive_operator: $ => choice('+', '-'),
---
+++ simpl.treesitter/queries/highlights.scm
@@ -0,0 +1,57 @@
+[
+  "contract"
+  "type"
+] @keyword.type
+
+[
+ "func"
+] @keyword.function
+
+(type_identifier) @type
+
+(function_declaration
+  name: (field_identifier) @function)
+
+(function_declaration
+  on_type: (type_identifier) @type)
+
+(param_declaration
+  name: (field_identifier) @variable.member)
+
+
+(call_expression
+  function: (field_identifier) @function.call)
+(param_definition
+  name: (field_identifier) @variable.parameter)
+
+(int_literal) @number
+(float_literal) @number.float
+(string_literal) @string
+[
+  "+"
+  "-"
+  "*"
+  "/"
+  "%"
+  "="
+  "<"
+  ">"
+  "<="
+  ">="
+  ; "^"
+  ; "&"
+  ; "&&"
+  "|"
+  ; "..."
+] @operator
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+] @punctuation.bracket
+
+(comment) @comment
