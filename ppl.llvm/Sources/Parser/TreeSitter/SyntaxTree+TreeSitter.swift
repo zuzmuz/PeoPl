86345e664ce0562008f35ff6a008f2c7b17ac4b0
zuzmuz <hamadehzaher0@gmail.com>
2025-01-29 12:04:38 +0200

    improved encoding for simple expressions

--- ppl.llvm/Sources/Parser/TreeSitter/SyntaxTree+TreeSitter.swift
+++ ppl.llvm/Sources/Parser/TreeSitter/SyntaxTree+TreeSitter.swift
@@ -380,6 +380,16 @@ extension Expression {
             self = .simple(simple)
         }
     }
+
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .simple(simple):
+            try container.encode(simple, forKey: .simple)
+        default:
+            break
+        }
+    }
 }
 
 extension Expression.Simple {
@@ -492,6 +502,50 @@ extension Expression.Simple {
             }
         default:
             return nil
+        }
+    }
+
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .intLiteral(value):
+            try container.encode(value, forKey: .intLiteral)
+        case let .floatLiteral(value):
+            try container.encode(value, forKey: .floatLiteral)
+        case let .stringLiteral(value):
+            try container.encode(value, forKey: .stringLiteral)
+        case let .boolLiteral(value):
+            try container.encode(value, forKey: .boolLiteral)
+        case let .positive(expression):
+            try container.encode(expression, forKey: .positive)
+        case let .negative(expression):
+            try container.encode(expression, forKey: .negative)
+        case let .plus(left, right):
+            try container.encode(["left": left, "right": right], forKey: .plus)
+        case let .minus(left, right):
+            try container.encode(["left": left, "right": right], forKey: .minus)
+        case let .times(left, right):
+            try container.encode(["left": left, "right": right], forKey: .times)
+        case let .by(left, right):
+            try container.encode(["left": left, "right": right], forKey: .by)
+        case let .equal(left, right):
+            try container.encode(["left": left, "right": right], forKey: .equal)
+        case let .different(left, right):
+            try container.encode(["left": left, "right": right], forKey: .different)
+        case let .lessThan(left, right):
+            try container.encode(["left": left, "right": right], forKey: .lessThan)
+        case let .lessThanEqual(left, right):
+            try container.encode(["left": left, "right": right], forKey: .lessThanEqual)
+        case let .greaterThan(left, right):
+            try container.encode(["left": left, "right": right], forKey: .greaterThan)
+        case let .greaterThanEqual(left, right):
+            try container.encode(["left": left, "right": right], forKey: .greaterThanEqual)
+        case let .or(left, right):
+            try container.encode(["left": left, "right": right], forKey: .or)
+        case let .and(left, right):
+            try container.encode(["left": left, "right": right], forKey: .and)
+        default:
+            break
         }
     }
 }
