434ac2895ab34c15f3282e3022596288754009f1
zuzmuz <hamadehzaher0@gmail.com>
2025-05-14 01:32:25 +0300

    finished new grammar without meta parameters

--- ppl.treesitter/grammar.js
+++ ppl.treesitter/grammar.js
@@ -31,7 +31,7 @@ module.exports = grammar({
   rules: {
     source_file: $ => repeat(
       seq(
-        $._statement,
+        $.field,
       )
     ),
 
@@ -50,157 +50,228 @@ module.exports = grammar({
     // -----------
      
     identifier: $ => /[A-Za-z_][A-Za-z-0-9_]*/,
-    keyword: $ => choice(
-      "'record",
-      "'choice",
-      "'set",
-      "'in",
-      "'any",
-      "'some"
-    ),
 
     field: $ => seq(
       field("name", $.identifier),
       ":",
-      field("type", optional($._type)),
+      field("definition", $.expression),
     ),
 
     field_list: $ => seq(
-      '[',
-      optional(seq(
+      seq(
         choice(
           $.field,
-          $._type
+          $.expression
         ),
         repeat(seq(
           ',',
           choice(
             $.field,
-            $._type
+            $.expression
           )
         )),
         optional(',')
-      )),
-      ']'
+      ),
     ),
 
-    // constrained_type: $ => seq(
-    //   field("name", $.identifier),
-    //   ':',
-    //   "'in",
-    //   field("set", $.identifier)
-    // ),
-    //
-    // _type_argument: $ => choice(
-    //   $.identifier,
-    //   $.labeled_type,
-    //   $.constrained_type
-    // ),
-    //
-    // type_arguments: $ => seq(
-    //   '<',
-    //     repeat(seq($._type_argument, ',')),
-    //     $._type_argument,
-    //   '>'
-    // ),
-
-    _definition: $ => choice(
-        $.record_definition,
-        $.choice_definition,
-        // $.function_definition,
-        // $.set_definition,
-        // $.implementation_definition
+    squared_field_list: $ => seq(
+      '[',
+        optional($.field_list),
+      ']',
     ),
 
-    _statement: $ => seq(
-      $._definition,
-    ),
-    
-    // Type Definitions
-    // ----------------
-  
-    record_definition: $ => seq(
-      field('name', $.identifier),
-      ':',
-      'record',
-      field('fields', $.field_list)
+    // Expression
+    // ----------
+
+    expression: $ => choice(
+      $._simple_expression,
+      $.function_definition,
+      $.branched_expression,
+      $.piped_expression
     ),
 
-    choice_definition: $ => seq(
-      field('name', $.identifier),
-      ':',
-      'choice',
-      field('variants', $.field_list)
+    call_expression: $ => seq(
+      field("prefix", $._simple_expression),
+      field("arguments", $.squared_field_list),
     ),
 
     // Function Definitions
     // --------------------
     
-    // function_definition: $ => seq(
-    //   field("signature", $.function_signature),
-    //   field("body", optional($.function_body)),
-    // ),
-    //
-    // function_signature: $ => seq(
-    //   "'func",
-    //   optional(seq('(', field("input_type", $._type), ')')),
-    //   field('arguments', $.labeled_types),
-    //   '->',
-    //   field("output_type", $._type),
-    // ),
-    //
-    // function_body: $ => seq(
-    //   '{',
-    //     $._expression,
-    //   '}'
-    // ),
+    function_definition: $ => prec.left(seq(
+      field("signature", $.function_signature),
+      field("body", optional($.function_body)),
+    )),
 
-    // Set Definitions
-    // ---------------
-    
-    // set_definition: _ => "'set",
-
-    // Implementation Definitions
-    // --------------------------
-    
-    // implementation_definition: $ => seq(
-    //   "'in",
-    //   $.identifier
-    // ),
-    //
-    // Types
-    // -----
-    //
-
-    // existential_type: $ => seq(
-    //   "'any",
-    //   $.nominal_type
-    // ),
-    //
-    // opaque_type: $ => seq(
-    //   "'some",
-    //   $.nominal_type
-    // ),
-    //
-    _type: $ => choice(
-      $.nothing,
-      $.never,
-      $.identifier,
-      // $.record_definition,
-      // $.choice_definition,
+    function_signature: $ => seq(
+      $.func,
+      optional(seq('(', field("input_type", $.expression), ')')),
+      optional(field('arguments', $.squared_field_list)),
+      optional(seq('->', field("output_type", $.expression))),
     ),
-    //
-    // nominal_type: $ => choice(
-    //   field('identifier', $.identifier),
-    // ),
-    //
+
+    function_body: $ => seq(
+      '{',
+        $.expression,
+      '}'
+    ),
+
     nothing: _ => 'nothing',
     never: _ => 'never',
-    //
-    // // -----------
-    // // EXPRESSIONS
-    // // -----------
-    //
-    // _expression: $ => "hi"
+    tuple: _ => 'tuple',
+    record: _ => 'record',
+    union: _ => 'union',
+    choice: _ => 'choice',
+    func: _ => 'func',
+
+    special: $ => choice(
+      $.nothing,
+      $.never,
+      $.tuple,
+      $.record,
+      $.union,
+      $.choice,
+    ),
+
+    literal: $ => choice(
+      $.int_literal,
+      $.float_literal,
+      $.string_literal,
+      $.bool_literal,
+    ),
+
+    _simple_expression: $ => choice(
+      $.special,
+      $.literal,
+      $.unary_expression,
+      $.binary_expression,
+      $.squared_field_list,
+      $.identifier,
+      $.parenthisized_expression,
+      $.function_body,
+      $.call_expression,
+      $.access_expression,
+      $.binding
+    ),
+
+    parenthisized_expression: $ => seq(
+      '(',
+      $.expression,
+      ')',
+    ),
+
+    access_expression: $ => seq(
+      field("prefix", $._simple_expression),
+      '.',
+      field("field", $.identifier),
+    ),
+
+    binding: $ => seq('$', $.identifier),
+
+    // Literals
+    // --------
+    
+    int_literal: $ => token(choice(
+        /[0-9][0-9_]*/,
+        /0x[0-9a-fA-F_]+/,
+        /0b[01_]+/,
+        /0o[0-7_]+/,
+    )),
+
+    float_literal: $ => /\d+\.\d+/,
+    string_literal: $ => /"[^"]*"/,
+    bool_literal: $ => choice('true', 'false'),
+
+    // Operators
+    // ---------
+
+    // a unary operator followed by a simple expression 
+    unary_expression: $ => prec.left(PREC.UNARY,
+      seq(
+        field("operator", choice(
+          $.multiplicative_operator,
+          $.additive_operator,
+          $.comparative_operator,
+          $.and_operator,
+          $.or_operator,
+          $.not_operator,
+        )),
+        field("operand", $._simple_expression),
+      )
+    ),
+    // two simple expressions surrounding a arithmetic or logic operator
+    binary_expression: $ => choice(
+      prec.left(PREC.MULT,
+        seq(
+          field("left", $._simple_expression),
+          field("operator", $.multiplicative_operator),
+          field("right", $._simple_expression),
+        )
+      ),
+      prec.left(PREC.ADD,
+        seq(
+          field("left", $._simple_expression),
+          field("operator", $.additive_operator),
+          field("right", $._simple_expression),
+        )
+      ),
+      prec.left(PREC.COMP,
+        seq(
+          field("left", $._simple_expression),
+          field("operator", $.comparative_operator),
+          field("right", $._simple_expression),
+        )
+      ),
+      prec.left(PREC.AND,
+        seq(
+          field("left", $._simple_expression),
+          field("operator", $.and_operator),
+          field("right", $._simple_expression),
+        )
+      ),
+      prec.left(PREC.OR,
+        seq(
+          field("left", $._simple_expression),
+          field("operator", $.or_operator),
+          field("right", $._simple_expression),
+        )
+      )
+    ),
+
+    multiplicative_operator: $ => choice('*', '/', '%'),
+    additive_operator: $ => choice('+', '-'),
+    comparative_operator: $ => choice('=', '!=', '>', '>=', '<', '<='),
+    not_operator: $ => 'not',
+    and_operator: $ => 'and',
+    or_operator: $ => 'or',
+
+    branched_expression: $ => prec.left(PREC.SUBPIPE, seq(
+      $.branch,
+      repeat(seq(',', $.branch)),
+    )),
+
+    branch: $ => seq(
+      '|', 
+      field("match_expression", $._simple_expression),
+      optional(seq(':', field("guard_expression", $._simple_expression))),
+      '|',
+      field("body", choice(
+        $._simple_expression,
+        $.looped_expression,
+      ))
+    ),
+
+    looped_expression: $ => seq(
+      $.parenthisized_expression,
+      '^'
+    ),
+
+    piped_expression: $ => prec.left(PREC.PIPE, seq(
+        field("left", $.expression),
+        field("operator", $.pipe_operator),
+        field("right", $.expression),
+    )),
+
+    pipe_operator: $ => '|>',
   }
 });
--- ppl.treesitter/queries/highlights.scm
+++ ppl.treesitter/queries/highlights.scm
@@ -1,87 +1,89 @@
 
 [
-  "record"
-  "choice"
-  ; "'in"
-  ; "'some"
-  ; "'any"
+ (tuple)
+ (record)
+ (union)
+ (choice)
 ] @keyword.type
 
-; [
-;  "'func"
-; ] @keyword.function
-
-; [
-;  "namespace"
-;  ] @include
-
-
+(func) @keyword.function
+;
+;
 ; (nominal_type
-;     (identifier) @type.definition)
+;     identifier: (identifier) @type.definition)
+;
+; ; (type_arguments
+; ;   (type_identifier
+; ;     (nominal_type
+; ;       (type_name) @variable.member)))
+(call_expression
+  prefix: (identifier) @function.call)
+(access_expression
+  field: (identifier) @variable.member)
 
-; (type_arguments
-;   (type_identifier
-;     (nominal_type
-;       (type_name) @variable.member)))
-
-
-; (type_arguments
-;   (["<" ">"] @punctuation.bracket))
-
+;
+;
+; ; (type_arguments
+; ;   (["<" ">"] @punctuation.bracket))
+;
+;
+(field
+  name: (identifier) @type.definition)
+;
+; (choice_definition
+;   name: (identifier) @type.definition)
+;
+; (set_definition
+;   name: (identifier) @type.definition)
+;
 ; (function_definition
-;   (function_signature
-;     (scoped_identifier
-;       (small_identifier) @function)))
-;
-;
-; (param_definition
-;   name: (small_identifier) @variable.member)
-;
-;
-; (function_call_expression
-;   prefix: (scoped_identifier
-;              (small_identifier) @function.call))
-;
-; (argument
-;   name: (small_identifier) @variable.member)
+;   name: (identifier) @function)
 
-; (int_literal) @number
-; (bool_literal) @boolean
-; (float_literal) @number.float
-; (string_literal) @string
-;
-; (multiplicative_operator) @operator
-; (additive_operator) @operator
-; (comparative_operator) @operator
-; (not_operator) @operator
-; (and_operator) @operator
-; (or_operator) @operator
-; (pipe_operator) @operator
-;
-; [
-;   "^"
-;   ","
-; ] @keyword.operator
-;
-; [
-;   "("
-;   ")"
-;   "["
-;   "]"
-;   "{"
-;   "}"
-; ] @punctuation.bracket
-;
-; [
-;  "::"
-;  ":"
-;  "."
-;  "=>"
-; ] @punctuation.delimiter
+
+
+
+(field_list
+  (field
+    name: (identifier) @variable.member))
+
+
+(int_literal) @number
+(bool_literal) @boolean
+(float_literal) @number.float
+(string_literal) @string
+
+(multiplicative_operator) @operator
+(additive_operator) @operator
+(comparative_operator) @operator
+(not_operator) @operator
+(and_operator) @operator
+(or_operator) @operator
+(pipe_operator) @operator
+
+[
+  "^"
+] @keyword.operator
+
+[
+  "("
+  ")"
+  "["
+  "]"
+  "{"
+  "}"
+  "|"
+] @punctuation.bracket
+
+[
+ ":"
+ "."
+ ","
+ "->"
+] @punctuation.delimiter
 
 (nothing) @constant.builtin
 (never) @constant.builtin
-
-; "_" @character.special
+;
+"$" @character.special
 
 (comment) @comment
