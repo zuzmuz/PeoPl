490cd8599d7742e8bf6b0ae2a51ea31d1b83546c
zuzmuz <hamadehzaher0@gmail.com>
2025-08-10 00:26:38 +0300

    adding llvm code back

--- ppl.core/Sources/Main/Backend/LLVM/LLVM.swift
+++ ppl.core/Sources/Main/Backend/LLVM/LLVM.swift
@@ -1,89 +1,90 @@
-// import cllvm
-// import Foundation
-//
-// public enum LLVM {}
-//
-// extension LLVM {
-//     public struct Builder: ~Copyable {
-//         let module: LLVMModuleRef
-//         let context: LLVMContextRef
-//         let builder: LLVMBuilderRef
-//
-//         var functions: [String: Function]
-//
-//         public init(name: String) {
-//             context = LLVMContextCreate()
-//             // initializing llvm
-//             LLVMInitializeAllTargets()
-//             LLVMInitializeAllTargetInfos()
-//             LLVMInitializeAllAsmPrinters()
-//             LLVMInitializeAllAsmParsers()
-//             LLVMInitializeAllTargetMCs()
-//             LLVMInitializeAllDisassemblers()
-//
-//             module = LLVMModuleCreateWithNameInContext(name, context)
-//             builder = LLVMCreateBuilderInContext(context)
-//
-//             functions = [:]
-//
-//         }
-//
-//         public func generate() -> String {
-//             return String(cString: LLVMPrintModuleToString(module))
-//         }
-//
-//         func save(to path: String) {
-//             LLVMPrintModuleToFile(module, path, nil)
-//         }
-//
-//         func verify() -> Bool {
-//             return  LLVMVerifyFunction(module, LLVMReturnStatusAction) != 0
-//         }
-//
-//         deinit {
-//             LLVMDisposeBuilder(builder)
-//             LLVMDisposeModule(module)
-//             LLVMContextDispose(context)
-//         }
-//     }
-//
-//     public protocol StatementBuilder {
-//         func llvmBuildStatement(llvm: inout Builder) throws(LLVM.Error)
-//     }
-//
-//     protocol TypeBuilder {
-//         // func llvmBuildType(llvm: inout Builder) -> LLVMTypeRef
-//         func llvmGetType(llvm: inout Builder) throws(LLVM.Error) -> LLVMTypeRef
-//     }
-//
-//     protocol ValueBuilder {
-//         func llvmBuildValue(
-//             llvm: inout Builder,
-//             // function: LLVM.Function,
-//             scope: borrowing [LLVM.ParamTag: LLVMValueRef?]
-//         ) throws(LLVM.Error) -> LLVMValueRef?
-//     }
-//
-//     public enum Error: LocalizedError {
-//         case notImplemented(String)
-//         case unreachable
-//     }
-//
-//     struct Function {
-//         let name: String
-//         let paramTypes: [LLVMTypeRef?]
-//         let paramNames: [LLVM.ParamTag: Int]
-//         let outputType: LLVMTypeRef
-//         let functionType: LLVMTypeRef
-//         let functionValue: LLVMValueRef
-//         // Mayby Body Expression
-//     }
-//
-//     enum ParamTag: Hashable {
-//         case named(String)
-//         case unnamed(UInt64)
-//         case input
-//     }
-// }
-//
-//     // -lc++ -stdlib=libc++  -L/opt/homebrew/Cellar/llvm/19.1.7/lib -Wl,-search_paths_first -Wl,-headerpad_max_install_names
+import cllvm
+import Foundation
+
+public enum LLVM {}
+
+extension LLVM {
+    public struct Builder: ~Copyable {
+        let module: LLVMModuleRef
+        let context: LLVMContextRef
+        let builder: LLVMBuilderRef
+
+        var functions: [String: Function]
+        // var 
+
+        public init(name: String) {
+            context = LLVMContextCreate()
+            // initializing llvm
+            LLVMInitializeAllTargets()
+            LLVMInitializeAllTargetInfos()
+            LLVMInitializeAllAsmPrinters()
+            LLVMInitializeAllAsmParsers()
+            LLVMInitializeAllTargetMCs()
+            LLVMInitializeAllDisassemblers()
+
+            module = LLVMModuleCreateWithNameInContext(name, context)
+            builder = LLVMCreateBuilderInContext(context)
+
+            functions = [:]
+
+        }
+
+        public func generate() -> String {
+            return String(cString: LLVMPrintModuleToString(module))
+        }
+
+        func save(to path: String) {
+            LLVMPrintModuleToFile(module, path, nil)
+        }
+
+        func verify() -> Bool {
+            return  LLVMVerifyFunction(module, LLVMReturnStatusAction) != 0
+        }
+
+        deinit {
+            LLVMDisposeBuilder(builder)
+            LLVMDisposeModule(module)
+            LLVMContextDispose(context)
+        }
+    }
+
+    public protocol StatementBuilder {
+        func llvmBuildStatement(llvm: inout Builder) throws(LLVM.Error)
+    }
+
+    protocol TypeBuilder {
+        // func llvmBuildType(llvm: inout Builder) -> LLVMTypeRef
+        func llvmGetType(llvm: inout Builder) throws(LLVM.Error) -> LLVMTypeRef
+    }
+
+    protocol ValueBuilder {
+        func llvmBuildValue(
+            llvm: inout Builder,
+            // function: LLVM.Function,
+            scope: borrowing [LLVM.ParamTag: LLVMValueRef?]
+        ) throws(LLVM.Error) -> LLVMValueRef?
+    }
+
+    public enum Error: LocalizedError {
+        case notImplemented(String)
+        case unreachable
+    }
+
+    struct Function {
+        let name: String
+        let paramTypes: [LLVMTypeRef?]
+        let paramNames: [LLVM.ParamTag: Int]
+        let outputType: LLVMTypeRef
+        let functionType: LLVMTypeRef
+        let functionValue: LLVMValueRef
+        // Mayby Body Expression
+    }
+
+    enum ParamTag: Hashable {
+        case named(String)
+        case unnamed(UInt64)
+        case input
+    }
+}
+
+    // -lc++ -stdlib=libc++  -L/opt/homebrew/Cellar/llvm/19.1.7/lib -Wl,-search_paths_first -Wl,-headerpad_max_install_names
