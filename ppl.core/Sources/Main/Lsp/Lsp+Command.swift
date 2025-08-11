33f36362bb810b51f96a7d36b574a92f21a5ce60
zuzmuz <hamadehzaher0@gmail.com>
2025-08-12 01:07:54 +0300

    made lsp server run as generic

--- ppl.core/Sources/Main/Lsp/Lsp+Command.swift
+++ ppl.core/Sources/Main/Lsp/Lsp+Command.swift
@@ -4,6 +4,58 @@ import Lsp
 import Utils
 
 extension Utils.LogLevel: ExpressibleByArgument {}
+
+// NOTE: these wrappers are stupid and are only useful for development process
+private actor WrapperHandler: Lsp.Handler {
+    let handler: any Lsp.Handler
+
+    init(handler: any Lsp.Handler) {
+        self.handler = handler
+    }
+
+    func handle(request: Lsp.RequestMessage) async -> Lsp.ResponseMessage {
+        return await self.handler.handle(request: request)
+    }
+
+    func handle(notification: Lsp.NotificationMessage) async {
+        await self.handler.handle(notification: notification)
+    }
+
+}
+
+private actor WrappedTransport: Lsp.Transport {
+
+    let transport: any Lsp.Transport
+
+    init(transport: any Lsp.Transport) {
+        self.transport = transport
+    }
+
+    func read() async throws -> Data {
+        return try await self.transport.read()
+    }
+
+    func write(_ data: Data) async throws {
+        try await self.transport.write(data)
+    }
+}
+
+private final class WrappedLogger: Utils.Logger {
+    let logger: any Utils.Logger
+
+    init(logger: any Utils.Logger) {
+        self.logger = logger
+    }
+
+    func log(level: Utils.LogLevel, tag: String, message: String) {
+        self.logger.log(level: level, tag: tag, message: message)
+    }
+
+    func log(level: Utils.LogLevel, tag: String, message: Data) {
+        self.logger.log(level: level, tag: tag, message: message)
+    }
+}
+
 extension Peopl {
     struct LspComand: AsyncParsableCommand {
         static let configuration = CommandConfiguration(
@@ -54,105 +106,61 @@ extension Peopl {
         @Option(name: .long, help: "logging level")
         var logLevel: Utils.LogLevel = .info
 
-        private static func getFileLogger(
-            filePath: String?,
-            level: Utils.LogLevel
-        ) throws -> some Utils.Logger {
-            guard let filePath else {
-                throw ValidationError(
-                    // swiftlint:disable:next line_length
-                    "File path for file logger is required when logger is set to file."
-                )
-            }
-            return try Utils.FileLogger(
-                filePath: URL(filePath: filePath),
-                level: level)
-        }
-
-        static func inplaceHandler<L: Utils.Logger>(
-            logger: L
-        ) -> PpLsp.Handler<L, TreeSitterModulParser> {
-            PpLsp.Handler(
-                moduleParser: TreeSitterModulParser.self,
-                logger: logger)
-        }
-
         func run() async throws {
 
-            switch (self.logger, self.transport, self.mode) {
-            case (.stdout, .std, _):
-                throw ValidationError(
-                    // swiftlint:disable:next line_length
-                    "logger stdout is not supported with std transport, logging needs to be on a separate transport"
-                )
-            case (.stderr, .std, .inplace):
-                let logger = Utils.StdErrLogger(level: self.logLevel)
-                let server = Lsp.Server(
-                    handler: Self.inplaceHandler(logger: logger),
-                    transport: Lsp.standardTransport,
-                    logger: logger)
-                try await server.run()
-            case (.file, .std, .inplace):
-                let logger = try Self.getFileLogger(
-                    filePath: self.filePath,
-                    level: self.logLevel)
-                let server = Lsp.Server(
-                    handler: Self.inplaceHandler(logger: logger),
-                    transport: Lsp.standardTransport,
-                    logger: logger)
-                try await server.run()
-            case (.stdout, .socket, .inplace):
-                let logger = Utils.ConsoleLogger(level: self.logLevel)
-                let tcpServer = try Socket.TcpServer(
-                    port: self.transportPort,
-                    logger: logger)
-                let server = Lsp.Server(
-                    handler: Self.inplaceHandler(logger: logger),
-                    transport: tcpServer,
-                    logger: logger)
-                try await tcpServer.start()
-                try await server.run()
-            case (_, .socket, _):
-                throw ValidationError(
-                    "socket transport requires inplace server and stdout"
-                )
-            case (.stderr, .std, .proxy):
-                let logger = Utils.StdErrLogger(level: self.logLevel)
-                let proxyHandler = try Lsp.ProxyHandler(
-                    client: try .init(
-                        port: self.proxyPort,
-                        host: self.proxyHost,
-                        logger: logger),
-                    logger: logger)
-                let server = Lsp.Server(
-                    handler: proxyHandler,
-                    transport: Lsp.standardTransport,
-                    logger: logger)
-
-                // run the proxy handler before running the server
-                // to start the tcp client
-                try await proxyHandler.run()
-                try await server.run()
-            case (.file, .std, .proxy):
-                let logger = try Self.getFileLogger(
-                    filePath: self.filePath,
-                    level: self.logLevel)
-                let proxyHandler = try Lsp.ProxyHandler(
-                    client: try .init(
-                        port: self.proxyPort,
-                        host: self.proxyHost,
-                        logger: logger),
-                    logger: logger)
-                let server = Lsp.Server(
-                    handler: proxyHandler,
-                    transport: Lsp.standardTransport,
-                    logger: logger)
-
-                // run the proxy handler before running the server
-                // to start the tcp client
-                try await proxyHandler.run()
-                try await server.run()
+            let logger: WrappedLogger
+            switch self.logger {
+            case .stderr:
+                logger = .init(
+                    logger: Utils.StdErrLogger(level: self.logLevel))
+            case .stdout:
+                logger = .init(
+                    logger: Utils.ConsoleLogger(level: self.logLevel))
+            case .file:
+                guard let filePath = self.filePath else {
+                    throw ValidationError(
+                        "file path is required for file logger")
+                }
+                logger = .init(
+                    logger: try Utils.FileLogger(
+                        filePath: URL(filePath: filePath),
+                        level: self.logLevel))
             }
+
+            let transport: WrappedTransport
+            switch self.transport {
+            case .std:
+                transport = .init(transport: Lsp.standardTransport)
+            case .socket:
+                let tcpServer = try Socket.TcpServer(
+                    port: self.transportPort, logger: logger)
+                transport = .init(
+                    transport: tcpServer)
+                try await tcpServer.start()
+            }
+
+            let mode: WrapperHandler
+            switch self.mode {
+            case .inplace:
+                mode = .init(
+                    handler: PpLsp.Handler(
+                        moduleParser: TreeSitterModulParser.self, logger: logger
+                    ))
+            case .proxy:
+                let tcpClient = try Socket.TcpClient(
+                    port: self.proxyPort, host: self.proxyHost, logger: logger)
+                let proxy = try Lsp.ProxyHandler(
+                    client: tcpClient, logger: logger)
+                mode = .init(
+                    handler: proxy)
+                try await tcpClient.start()
+            }
+
+            let server = Lsp.Server(
+                handler: mode,
+                transport: transport,
+                logger: logger)
+            try await server.run()
         }
     }
 }
