576b872346e8ed0bcb4b0781ab910da7f065f509
zuzmuz <hamadehzaher0@gmail.com>
2025-08-14 18:39:13 +0300

    adding definition context files to commit

---
+++ ppl.core/Sources/Main/Backend/LLVM/LLVM+Context.swift
@@ -0,0 +1,9 @@
+extension Semantic.DefinitionsContext {
+    func llvmBuild(
+        llvm: inout LLVM.Builder
+    ) throws(LLVM.Error) {
+        for (identifier, typeSpecifier) in self.typeDefinitions {
+
+        }
+    }
+}
---
+++ ppl.core/Sources/Main/Build.swift
@@ -0,0 +1,48 @@
+import ArgumentParser
+import Foundation
+
+extension Peopl {
+    struct BuildCommand: AsyncParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "build",
+            abstract: "Build the PeoPl project",
+            discussion: "Builds the PeoPl project using the specified backend."
+        )
+
+        @Flag(name: .shortAndLong, help: "output llvm ir")
+        var llvm: Bool = false
+
+        @Option(name: .shortAndLong, help: "output file path")
+        var output: String?
+
+        func run() async throws {
+            let fileManager = FileManager.default
+            let currentDirectory = URL(
+                filePath: fileManager.currentDirectoryPath)
+            print("building PeoPl project in: \(currentDirectory)")
+
+            let contents = try fileManager.contentsOfDirectory(
+                at: currentDirectory,
+                includingPropertiesForKeys: [.isRegularFileKey],
+                options: [.skipsHiddenFiles]
+            )
+
+            let modules: [String: Syntax.Module] =
+                try contents.reduce(into: [:]) { acc, url in
+                    guard url.pathExtension == "ppl" else { return }
+                    let source = try Syntax.Source(url: url)
+                    acc[url.absoluteString] =
+                        TreeSitterModulParser.parseModule(source: source)
+                }
+
+            let project = Syntax.Project(
+                modules: modules)
+
+            let context = try project.semanticCheck().get()
+
+            let llvmBuilder = LLVM.Builder(name: "PeoPl")
+
+            // context.definitions
+        }
+    }
+}
