45860d8e6914fa699ac863731ad0a26ed4d84620
zuzmuz <hamadehzaher0@gmail.com>
2025-08-22 19:06:55 +0300

    adding unnamed arguments tests

--- ppl.core/Tests/MainTests/FrontendTests/UnnamedArgumentsTests.swift
+++ ppl.core/Tests/MainTests/FrontendTests/UnnamedArgumentsTests.swift
@@ -2,8 +2,56 @@ import XCTest
 
 @testable import Main
 
-// final class UnnamedArgumentsTests: XCTestCase {
-// 	let filenNames:
-// 		[String: (
-// 		)]
-// }
+final class UnnamedArgumentsTests: XCTestCase {
+	let filenNames:
+		[String: (
+			typeDeclarations: Semantic.TypeDeclarationsMap,
+			expressionDefinitions: Semantic.FunctionDefinitionsMap
+		)] = [
+			"unnamedparams": (
+				typeDeclarations: [
+					.chain(["Pos"]): .raw(
+						.record([
+							.named("a"): .nominal(.chain(["Float"])),
+							.named("b"): .nominal(.chain(["Bool"])),
+							.unnamed(0): .nominal(.chain(["Int"])),
+							.unnamed(1): .nominal(.chain(["Float"])),
+						])
+					)
+				],
+				expressionDefinitions: [:]
+			)
+		]
+
+	func testFiles() throws {
+		let bundle = Bundle.module
+		let intrinsicDeclarations = Semantic.getIntrinsicDeclarations()
+
+		for (name, reference) in filenNames {
+			let sourceUrl = bundle.url(
+				forResource: "analyzer_\(name)",
+				withExtension: "ppl"
+			)!
+
+			let source = try Syntax.Source(url: sourceUrl)
+			let module = TreeSitterModulParser.parseModule(source: source)
+			let (typeDeclarations, typeLookup, _) =
+				module.resolveTypeSymbols(
+					contextTypeDeclarations: intrinsicDeclarations.typeDeclarations)
+			let allTypeDeclarations = intrinsicDeclarations.typeDeclarations
+				.merging(typeDeclarations) { $1 }
+
+			let (
+				functionDeclarations,
+				functionBodyExpressions,
+				_,
+				_
+			) = module.resolveFunctionSymbols(
+				typeLookup: typeLookup,
+				typeDeclarations: allTypeDeclarations,
+				contextFunctionDeclarations: intrinsicDeclarations
+					.functionDeclarations)
+
+		}
+	}
+}
