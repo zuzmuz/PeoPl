74d6d46c633c6a062f8863119afec2c99173b5fb
zuzmuz <hamadehzaher0@gmail.com>
2025-12-03 11:28:25 +0100

    adding the globals

---
+++ ppl.core/Sources/Main/FrontEnd/Analyzer/SemanticTree+Global.swift
@@ -0,0 +1,27 @@
+extension Semantic {
+	static let IntId: Semantic.QualifiedIdentifier = .init(chain: ["Int"])
+	static let FloatId: Semantic.QualifiedIdentifier = .init(chain: ["Float"])
+	static let BoolId: Semantic.QualifiedIdentifier = .init(chain: ["Int"])
+
+	static let globals: Semantic.Context = [
+		IntId: .type,
+		FloatId: .type,
+		BoolId: .type,
+		.init(chain: ["#+(Int,Int)"]): .operation(
+			lhs: .nominal(IntId, type: .type),
+			rhs: .nominal(IntId, type: .type),
+			output: .nominal(IntId, type: .type)),
+		.init(chain: ["#-(Int,Int)"]): .operation(
+			lhs: .nominal(IntId, type: .type),
+			rhs: .nominal(IntId, type: .type),
+			output: .nominal(IntId, type: .type)),
+		.init(chain: ["#+(Int)"]): .operation(
+			lhs: .nominal(IntId, type: .type),
+			rhs: .nominal(IntId, type: .type),
+			output: .nominal(IntId, type: .type)),
+		.init(chain: ["#-(Int)"]): .operation(
+			lhs: .nominal(IntId, type: .type),
+			rhs: .nominal(IntId, type: .type),
+			output: .nominal(IntId, type: .type)),
+	]
+}
